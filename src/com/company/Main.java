449415f612f81e58b7693369d518643d71d1f3de
OmerErkan00 <141404465+OmerErkan00@users.noreply.github.com>
2023-08-25 12:35:47 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        String hello = "Hello world";
-
-        System.out.println(hello);
+        deneme
     }
 }
