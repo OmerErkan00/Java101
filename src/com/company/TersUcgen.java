84c7891ab573ccb0cb945b17bf26711ccee2cf8f
OmerErkan00 <141404465+OmerErkan00@users.noreply.github.com>
2023-09-07 21:01:54 +0300

    Create TersUcgen.java

---
+++ src/com/company/TersUcgen.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        for (int i = 9; i >= 0; i--){
+            for (int j = 0; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
